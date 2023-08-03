c34fb078c6bbd4616192d75eee492b8ea0326514
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-08-02 21:25:35 -0300

    Interface user repository

---
+++ src/main/java/br/com/lucas/api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.lucas.api.repositories;
+
+import br.com.lucas.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
