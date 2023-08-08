b1afea7e25301128e4d65dc2b2d6a343cefee796
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2023-08-08 20:40:17 -0300

    Retornando DTO para User Resourse

--- src/main/java/br/com/lucas/api/resources/UserResource.java
+++ src/main/java/br/com/lucas/api/resources/UserResource.java
@@ -1,7 +1,9 @@
 package br.com.lucas.api.resources;
 
 import br.com.lucas.api.domain.User;
+import br.com.lucas.api.domain.dto.UserDTO;
 import br.com.lucas.api.services.users.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
     private UserService userService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         System.out.println("Find by id: " + id);
-        return ResponseEntity.ok().body(userService.findById(id));
+        return ResponseEntity.ok().body(modelMapper.map(userService.findById(id), UserDTO.class));
     }
 }
